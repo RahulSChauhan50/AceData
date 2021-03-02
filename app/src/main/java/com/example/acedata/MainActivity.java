5c8215ab4072f9b418417e8dc2b92647d3a9e278
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-03-02 22:52:31 +0530

    permission

--- app/src/main/java/com/example/acedata/MainActivity.java
+++ app/src/main/java/com/example/acedata/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.acedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +19,15 @@ import com.example.acedata.ui.Pin_Generator;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     FragmentManager fragmentManager;
+    final int ALL_PERMISSION=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askPermisson_function();
+
          fragmentManager = getFragmentManager();
 
 //        Intent intent=new Intent(this,AppActivity.class);
@@ -59,5 +66,19 @@ public class MainActivity extends AppCompatActivity {
         Pin_Fragment pin = new Pin_Fragment();
         trans.replace(R.id.main_Frame,pin);
         trans.commit();
+    }
+    public void askPermisson_function(){
+        String[] permissions={Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.CAMERA};
+
+        for(String permission:permissions){
+            if(ActivityCompat.checkSelfPermission(MainActivity.this,permission)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, permissions, ALL_PERMISSION);
+                break;
+            }
+        }
+
     }
 }

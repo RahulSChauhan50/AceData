bea414302f05f7849a632ee64121bf5fd10a48d7
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-03-03 21:03:46 +0530

    Added Fragment library

--- app/src/main/java/com/example/acedata/AppActivity.java
+++ app/src/main/java/com/example/acedata/AppActivity.java
@@ -8,6 +8,7 @@ import android.widget.ProgressBar;
 
 import com.example.acedata.network.RetrofitClientInstance;
 import com.example.acedata.network.UploadReceiptService;
+import com.example.acedata.ui.datalist.DatalistFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -28,43 +29,17 @@ import retrofit2.Callback;
 
 public class AppActivity extends AppCompatActivity {
 
-    ProgressBar progressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
 
-        progressBar=findViewById(R.id.progressBar2);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, DatalistFragment.class, null)
+                    .commit();
+        }
 
-        String Token = "Token 9dc7a0c191f9e74cbbd3fd15731a60bb23ee7073";
-
-        UploadReceiptService service = RetrofitClientInstance.getRetrofitInstance().create(UploadReceiptService.class);
-
-        Call<List<FormData>> call = service.getDataList(Token);
-        progressBar.setVisibility(View.VISIBLE);
-        call.enqueue(new Callback<List<FormData>>() {
-            @Override
-            public void onResponse(Call<List<FormData>> call, retrofit2.Response<List<FormData>> response) {
-                List<FormData> posts = response.body();
-                    progressBar.setVisibility(View.INVISIBLE);
-                for ( FormData post : posts) {
-                    String content = "";
-                    content += "name: " + post.getName() + "\n";
-                    content += "Address: " + post.getAddress() + "\n";
-                    content += "adhar: " + post.getAdhar() + "\n";
-                    content += "mobile: " + post.getMobile_no() + "\n\n";
-
-                    Log.d("Content",content);
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<FormData>> call, Throwable t) {
-                Log.d("Faied","Network request failed");
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-        });
     }
 }
